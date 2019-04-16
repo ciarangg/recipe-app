b309eead909e7cc69d3110480a2518a5ad252784
Ciaran <cguhagil@terpmail.umd.edu>
2019-04-16 11:43:39 -0600

    page populating correctly

--- src/Recipe.js
+++ src/Recipe.js
@@ -7,12 +7,16 @@ class Recipe extends Component {
         let ingredients = info.ingredients
         console.log(ingredients)
 
+        const ingredientsList = ingredients.map((ingredientList) => {
+            return (<li> {ingredientList} </li> )
+            })
+
         return ( <div>
                     <h1> {info.title} </h1>
                     <img src={info.picture} alt={info.title} height="350" width="350" />
                     <p> <b>Ingredients:</b> {info.ingredients} </p>
                     <ul>
-                        
+                        {ingredientsList}
                     </ul>
                     <p> <b> Recipe: </b> {info.instructions} </p>
 
