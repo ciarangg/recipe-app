dbbd0df25b1ecb4db7f3648be464200a6cac95a1
Ciaran <cguhagil@terpmail.umd.edu>
2019-04-15 21:03:31 -0600

    populating page with first object in json file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Recipe from './Recipe';
+import recipelist from './recipelist.json'
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Recipe />
+        <Recipe json={recipelist} />
       </div>
     );
   }
--- src/Recipe.js
+++ src/Recipe.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 class Recipe extends Component {
     render() {
-        return ( <div> Recipe </div>
+        let {json} = this.props
+        let firstObject = json[0]
+        console.log(firstObject.picture)
+        return ( <div>
+                    <h1> {firstObject.title} </h1>
+                    <img src={firstObject.picture} alt={firstObject.title} />
+                    <p> <b>Ingredients:</b> </p>
+                    <ul>
+                        <li>{firstObject.ingredients}</li>
+                        <li>{firstObject.ingredients}</li>
+                        <li>{firstObject.ingredients}</li>
+                    </ul>
+                    <p> <b> Recipe: </b> {firstObject.instructions} </p>
+
+                </div>
         );
     }
 }
